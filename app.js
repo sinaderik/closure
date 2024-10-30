82f9351aa74e9e9331f776fee5f735a9171ab38c
Sina <sinaderik7@gmail.com>
2024-10-30 17:20:01 +0330

    counter function is added

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-function outer(name){
-    // outer function is invoked and finished
-    console.log("outer function")
-    function inner(){
-        // 'keep the state even after the outer function is finished'
-        console.log(`Hi my name is ${name}`)
-    }
-    inner()
+function outer(name) {
+  // outer function is invoked and finished
+  console.log("outer function");
+  function inner() {
+    // 'keep the state even after the outer function is finished'
+    console.log(`Hi my name is ${name}`);
+  }
+  inner();
 }
-outer('sina')
+outer("sina");
 
+function createCounter(number = 0) {
+  return function plus() {
+    return ++number;
+  };
+}
+const counter = createCounter(1);
+
+console.log(counter());
+console.log(counter());
+
+function counterWithoutClosure(number) {
+  return ++number;
+}
+const normalCounter = counterWithoutClosure(10);
+console.log(normalCounter);
+console.log(normalCounter);
